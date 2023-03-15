629bbcdc4c80a57b0222bcacab99ce8d42db999e
Emma Kellett <emmakellett@AdminisatorsMBP.fritz.box>
2023-03-15 15:21:02 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
