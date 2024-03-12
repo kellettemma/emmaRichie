0b1519e6275ec7e3810f5ba378904e88937b9525
Emma Kellett <emmakellett@AdminisatorsMBP.fritz.box>
2024-03-12 21:39:44 +0000

    rsvp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,113 @@
 import * as React from "react"
+import "./styles.css" 
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main>
+      <div class="masthead">
+        <div class="details">
+          <h1><img src="./images/emma-richie.png" alt="Emma and Richard" /></h1>
+          <p class="sub-heading">Together with Isabelle and their families</p>
+          <p class="sub-heading">invite you to share in their wedding celebration</p>
+          <p class="sub-heading">SUNDAY, the 30th of JUNE 2024 <br />at 2:30pm at Castle Leslie Glaslough, Co. Monaghan</p>
+          <div class="letter-image fadeIn">
+            <div class="animated-mail">
+              <div class="back-fold"></div>
+              <div class="letter"><img src="./images/wedding-invitation.jpg" /></div>
+              <div class="top-fold"></div>
+              <div class="body"></div>
+              <div class="left-fold"></div>
+            </div>
+          </div>
+          <a class="button fadeIn" href="#rsvp">RSVP</a>
+        </div>
+        <img src="./images/castle.png" alt="castle leslie" class="masthead-mobile" />
+      </div>
+      <div class="parallax__img"></div>
+      <section>
+        <div class="blue">
+          <div class="flex fadeIn">
+            <h2>Guest Information</h2>
+            <h3>The Ceremony</h3>
+            <p>The ceremony will take place at 2:30pm outdoors on Castle Grounds (weather permitting), otherwise, it will take place inside the castle. </p>
+            <h3>Wedding reception</h3>
+            <p>Join us in the castle after the ceremony for a drinks reception, dinner, cake and dancing!</p>
+            <p>In order to allow all guests, including parents, an evening of relaxation we have chosen for our wedding day to be an adult-only occasion. We hope this advance notice means you are still able to share our big day and will enjoy having the evening off!</p>
+            <h3> Accommodation</h3>
+            <p>We have reserved rooms for the bridal party and immediate family. There are additional rooms for guests, please get in touch with us and we will be happy to reserve a room for you.</p>
+            <p>Alternatively, you can contact the Estate directly to enquire about availability and rates or book directly on the website, <a href="https://www.castleleslie.com/" target="_blank">www.castleleslie.com</a></p><p>For more information on the local area please see <a href="https://www.castleleslie.com/attractions/walking/" target="_blank">castle leslie local area section</a>. For more information on walks around the castle please see <a href="https://www.castleleslie.com/attractions/walking/" target="_blank">castle leslie walking page</a></p>
+            <h3> Social Media</h3>
+            <p>We politely ask that our guests don't share photos of our wedding on social media- thank you for your understanding.</p>
+            <h3> Location</h3>
+            <p>Castle Leslie Estate, <br />Glaslough,<br />Co.Monaghan,<br />H18 FY04, Ireland.</p>
+            <p> <strong><a href="https://www.castleleslie.com/contact/directions/" target="_blank">Driving directions to Castle Leslie Estate</a></strong>.<br /><strong>Sat Nav:</strong> Latitude 54.31821, Longitude -6.89582<br /><strong>Garmin Loc8 code:</strong> G6E-22-5TK</p>
+          </div>
+        </div></section>
+      <div class="parallax__img parallax__img--2"></div>
+      <footer>
+        <div class="flex fadeIn">
+          <h2 id="rsvp">Kindly reply by April 20<sup>th</sup></h2>
+          <p>Kindly RSVP by filling out the form below. </p>
+          <p> We are so excited to celebrate with you!</p>
+          <form id="fs-frm" name="basic-rsvp-form" accept-charset="utf-8" action="https://formspree.io/f/xrgnlbrp" method="post">
+            <fieldset id="fs-frm-inputs" class="flex-row">
+              <div class="column">
+                <label for="full-name">Full Name</label>
+                <input type="text" name="name" id="full-name" placeholder="First and Last" />
+                <label for="attending">Will you Attend?</label>
+                <select name="attending" id="attending">
+                  <option value="">Select</option>
+                  <option value="Yes">Gladly accepts</option>
+                  <option value="No">Regretfully declines</option>
+                </select>
+                <label for="main">What main course?</label>
+                <select name="main" id="main">
+                  <option value="">Select</option>
+                  <option value="beef">Traditional Beef Wellington, Roscoff Onion, Fondant Potato, Heritage Carrot &amp; Vegetable Parcel, Madeira Jus</option>
+                  <option value="fish">Pan-Fried Hake, Heritage Carrot &amp; Vegetable Parcel, Creamed Potato, With wine Velouté</option>
+                </select>
+                <label for="diet">Do you have any special dietiary requirements?</label>
+                <textarea name="diet" id="diet"></textarea>
+                <label for="song">Any special song requests?</label>
+                <textarea name="song" id="song"></textarea>
+                <label for="message">Questions or Comments</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <div class="column">
+                <label for="plus">Are you bringing a guest?</label
+                ><select name="plus" id="plus">
+                  <option value="">Select</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+                <label for="guest-full-name">Guest Full Name</label>
+                <input type="text" name="guest-full-name" id="guest-full-name" placeholder="First and Last" />
+                <label for="guest-main">What main course?</label>
+                <select name="guest-main" id="guest-main">
+                  <option value="">Select</option>
+                  <option value="beef">Traditional Beef Wellington, Roscoff Onion, Fondant Potato, Heritage Carrot &amp; Vegetable Parcel, Madeira Jus</option>
+                  <option value="fish">Pan-Fried Hake, Heritage Carrot &amp; Vegetable Parcel, Creamed Potato, With wine Velouté</option>
+                </select>
+                <label for="guest-diet">Do they have any special dietiary requirements?</label>
+                <textarea name="guest-diet" id="guest-diet"></textarea>
+                <label for="accommodation">Would you like to reserve a room?</label>
+                <select name="accommodation" id="accommodation"
+                ><option value="">Select</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+              </div>
+              <input type="hidden" name="_subject" id="email-subject" value="RSVP" />
+            </fieldset>
+            <input type="submit" value="RSVP" />
+          </form>
+        </div>
+      </footer>
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Emma & Richard </title>
