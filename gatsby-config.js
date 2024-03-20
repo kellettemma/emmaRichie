e18c0bf655426daf22658cb94ef822f0f8ded962
Emma Kellett <emmakellett@AdminisatorsMBP.fritz.box>
2024-03-20 21:07:25 +0000

    test deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/emmaRichie",
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
